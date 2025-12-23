68eb074b6727547606f562a6fb2f22087bc19649
Mike H <mikehealy@outlook.com>
2025-12-23 13:04:12 +0000

    Added basic search bar to hompage. Will need to add services/api stuff to do anythign else really.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonIcon, IonLabel, IonItem, IonInput } from '@ionic/angular/standalone';
 import { RouterLink } from '@angular/router';
 import { addIcons } from 'ionicons';
-import { heart, settings, home } from 'ionicons/icons';
+import { heart, settings, home, search } from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonIcon, IonLabel, RouterLink],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonIcon, IonLabel, IonItem, IonInput, RouterLink],
 })
 export class HomePage {
   constructor() {
-    addIcons({ heart, settings, home });
+    addIcons({ heart, settings, home, search });
   }
 }
